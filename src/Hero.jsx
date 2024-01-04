59a350d75027de414b8f3d17b2cc7a5fe5f40851
leslientodi <lesliehazard500@gmail.com>
2024-01-04 11:54:12 +0000

    added photogrid

---
+++ src/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function Grid () {
+    return (
+        <>
+            <div className="photogrid">
+                <img src="../src/images/photogrid.jpeg" width='800px'></img>
+            </div>
+            <div className="text">
+                <h1>Online Experiences</h1>
+                <small>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</small>
+
+            </div>
+        
+        </>
+        
+    )
+
+}
---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function NavBar (){
+    return (
+        <div className="navbar">
+            <img src="../src/images/airbnb.jpeg" width='70px'></img>
+            
+        </div>
+        
+    )
+}
+
